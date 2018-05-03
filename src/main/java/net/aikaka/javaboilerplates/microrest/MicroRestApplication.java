db9192e0eb1fa54c729a605320ffc4592271b9d8
Ai Nguyen <ainguyenkaka@gmail.com>
2018-05-03 15:25:35 +0800

    add spring boot admin

--- src/main/java/net/aikaka/javaboilerplates/microrest/MicroRestApplication.java
+++ src/main/java/net/aikaka/javaboilerplates/microrest/MicroRestApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicroRestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroRestApplication.class, args); //NOSONAR
+        SpringApplication.run(MicroRestApplication.class, args);
     }
 }
